a8766edc1abd231f2b3c59b439feeb676c18f9ec
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-12-13 12:17:16 +0900

    learn function

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,30 @@
 window.addEventListener("load", () => {
 
-  // あなたの手の画像
   const yourHandImage = document.querySelector(".cardBox__img--you");
-
-  // あなたの手のテキスト
   const yourHandText = document.querySelector(".cardBox__hand--you");
-
-  // CPUの手の画像
   const cpuHandImage = document.querySelector(".cardBox__img--cpu");
-
-  // CPUの手のテキスト
   const cpuHandText = document.querySelector(".cardBox__hand--cpu");
-
-  // カードのタイトル
   const cardTitle = document.querySelector(".card__title");
-
-  // カードの結果
   const cardResult = document.querySelector(".card__result");
-
-  // じゃんけんのボタンリスト
   const navigationHandList = document.querySelector(".navigation__list--hands");
-
-  // もう一度ボタンのリスト
   const navigationRestartList = document.querySelector(".navigation__list--restart");
 
-  // もう一度ボタンを最初は非表示にする
-  navigationRestartList.style.display = "none";
+  const displayYourHand = (image, text) => {
+    yourHandImage.src = image;
+    yourHandText.innerHTML = text;
+  };
 
-  // グーを押した時
-  document.querySelector(".navigation__button--rock").addEventListener("click", () => {
+  const displayCpuHand = (image, text) => {
+    cpuHandImage.src = image;
+    cpuHandText.innerHTML = text;
+  }
 
-    // あなたの出した手
-    yourHandImage.src = "./assets/images/rock.png";
-    yourHandText.innerHTML = "グー";
-
-    // CPUの出した手
-    cpuHandImage.src = "./assets/images/scissors.png";
-    cpuHandText.innerHTML = "チョキ";
-
-    // じゃんけん後のカードの結果反映
+  const result = () => {
     cardTitle.innerHTML = "ぽんっ！"
     cardResult.innerHTML = "あなたの勝ち！";
+  }
 
-    // ボタンの表示切り替え
-    navigationHandList.style.display = "none";
-    navigationRestartList.style.display = "flex";
-  });
-
-  // チョキを押した時
-  document.querySelector(".navigation__button--scissors").addEventListener("click", () => {
-    yourHandImage.src = "./assets/images/scissors.png";
-    yourHandText.innerHTML = "チョキ";
-    cpuHandImage.src = "./assets/images/paper.png";
-    cpuHandText.innerHTML = "パー";
-    cardTitle.innerHTML = "ぽんっ！"
-    cardResult.innerHTML = "あなたの勝ち！";
-    navigationHandList.style.display = "none";
-    navigationRestartList.style.display = "flex";
-  });
-
-  // パーを押した時
-  document.querySelector(".navigation__button--paper").addEventListener("click", () => {
-    yourHandImage.src = "./assets/images/paper.png";
-    yourHandText.innerHTML = "パー";
-    cpuHandImage.src = "./assets/images/rock.png";
-    cpuHandText.innerHTML = "グー";
-    cardTitle.innerHTML = "ぽんっ！"
-    cardResult.innerHTML = "あなたの勝ち！";
-    navigationHandList.style.display = "none";
-    navigationRestartList.style.display = "flex";
-  });
-
-  document.querySelector(".navigation__button--restart").addEventListener("click", () => {
-
+  const reset = () => {
     // 出した手のところを考え中に戻す
     yourHandImage.src = "./assets/images/thinking.png";
     yourHandText.innerHTML = "考え中...";
@@ -86,5 +38,47 @@ window.addEventListener("load", () => {
     // ボタンの表示切り替え
     navigationHandList.style.display = "flex";
     navigationRestartList.style.display = "none";
+  };
+
+  const navigationHandHide = () => {
+    navigationHandList.style.display = "none";
+    navigationRestartList.style.display = "flex";
+  };
+
+  // もう一度ボタンを最初は非表示にする
+  navigationRestartList.style.display = "none";
+
+  // グーを押した時
+  document.querySelector(".navigation__button--rock").addEventListener("click", () => {
+
+    // あなたの出した手
+    displayYourHand("./assets/images/rock.png", "グー");
+
+    // CPUの出した手
+    displayCpuHand("./assets/images/scissors.png", "チョキ");
+
+    // じゃんけん後のカードの結果反映
+    result();
+
+    // ボタンの表示切り替え
+    navigationHandHide();
   });
+
+  // チョキを押した時
+  document.querySelector(".navigation__button--scissors").addEventListener("click", () => {
+    displayYourHand("./assets/images/scissors.png", "チョキ");
+    displayCpuHand("./assets/images/paper.png", "パー");
+    result();
+    navigationHandHide();
+  });
+
+  // パーを押した時
+  document.querySelector(".navigation__button--paper").addEventListener("click", () => {
+    displayYourHand("./assets/images/paper.png", "パー");
+    displayCpuHand("./assets/images/rock.png", "グー");
+    result();
+    navigationHandHide();
+  });
+
+  document.querySelector(".navigation__button--restart").addEventListener("click", () => reset());
 });
