d832894e7c64e9f3544689163ec859f0f50e2162
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-12-14 09:53:40 +0900

    fix

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,7 @@ window.addEventListener("load", () => {
   const cardResult = document.querySelector(".card__result");
   const navigationHandList = document.querySelector(".navigation__list--hands");
   const navigationRestartList = document.querySelector(".navigation__list--restart");
-  const rockButton = document.querySelector(".navigation__button--rock");
-  const scissorsButton = document.querySelector(".navigation__button--scissors");
-  const paperButton = document.querySelector(".navigation__button--paper");
-  const restartButton = document.querySelector(".navigation__button--restart");
+  const handButtons = navigationHandList.querySelectorAll(".navigation__button");
 
   const displayYourHand = (param, text) => {
     yourHandImage.src = `./assets/images/${param}.png`;
@@ -53,14 +50,15 @@ window.addEventListener("load", () => {
   // もう一度ボタンを最初は非表示にする
   navigationRestartList.style.display = "none";
 
-  const handButtons = [rockButton, scissorsButton, paperButton];
+  const handParams = ["rock", "scissors", "paper"];
+  const handTextList = ["グー", "チョキ", "パー"];
   for(let i = 0; i < handButtons.length; i++) {
 
     // グー、チョキ、パーそれぞれのボタン
     const button = handButtons[i];
     button.addEventListener("click", () => {
-      const yourHand = button.dataset.hand;
-      const yourText = button.innerHTML;
+      const yourHand = handParams[i];
+      const yourText = handTextList[i];
 
       // 0~2のランダムな数字の取得
       // CPUの手をランダムに決める
@@ -75,7 +73,7 @@ window.addEventListener("load", () => {
     });
   }
 
-  restartButton.addEventListener("click", () => {
+  document.querySelector(".navigation__button--restart").addEventListener("click", () => {
 
     // 出した手のところを考え中に戻す
     displayReset();
