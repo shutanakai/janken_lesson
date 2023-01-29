2222057a90c66572cd2ed50a1ee0f095ef7b059b
t5a1r1ou <baw.keepon69@gmail.com>
2023-01-29 17:42:48 +0900

    もういちどボタン削除

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,9 @@ window.addEventListener("load", () => {
   const cpuHandText = document.querySelector(".cardBox__hand--cpu");
   const cardTitle = document.querySelector(".card__title");
   const cardResult = document.querySelector(".card__result");
-  const navigationHandList = document.querySelector(".navigation__list--hands");
-  const navigationRestartList = document.querySelector(".navigation__list--restart");
   const rockButton = document.querySelector(".navigation__button--rock");
   const scissorsButton = document.querySelector(".navigation__button--scissors");
   const paperButton = document.querySelector(".navigation__button--paper");
-  const restartButton = document.querySelector(".navigation__button--restart");
 
   const displayYourHand = (param, text) => {
     yourHandImage.src = `./assets/images/${param}.png`;
@@ -23,35 +20,10 @@ window.addEventListener("load", () => {
     cpuHandText.innerHTML = text;
   };
 
-  const displayReset = () => {
-    yourHandImage.src = "./assets/images/thinking.png";
-    yourHandText.innerHTML = "かんがえちゅう...";
-    cpuHandImage.src = "./assets/images/thinking.png";
-    cpuHandText.innerHTML = "かんがえちゅう...";
-  };
-
   const result = () => {
     cardTitle.innerHTML = "ぽんっ！"
     cardResult.innerHTML = "あなたのかち！";
   }
-
-  const navigationHandHide = () => {
-    navigationHandList.style.display = "none";
-    navigationRestartList.style.display = "flex";
-  };
-
-  const navigationRestartHide = () => {
-    navigationHandList.style.display = "flex";
-    navigationRestartList.style.display = "none";
-  }
-
-  const cardReset = () => {
-    cardTitle.innerHTML = "じゃんけん...";
-    cardResult.innerHTML = "";
-  }
-
-  // もう一度ボタンを最初は非表示にする
-  navigationRestartList.style.display = "none";
 
   // グーを押した時
   rockButton.addEventListener("click", () => {
@@ -64,9 +36,6 @@ window.addEventListener("load", () => {
 
     // じゃんけん後のカードの結果反映
     result();
-
-    // ボタンの表示切り替え
-    navigationHandHide();
   });
 
   // チョキを押した時
@@ -74,7 +43,6 @@ window.addEventListener("load", () => {
     displayYourHand("scissors", "チョキ");
     displayCpuHand("paper", "パー");
     result();
-    navigationHandHide();
   });
 
   // パーを押した時
@@ -82,19 +50,6 @@ window.addEventListener("load", () => {
     displayYourHand("paper", "パー");
     displayCpuHand("rock", "グー");
     result();
-    navigationHandHide();
   });
 
-  restartButton.addEventListener("click", () => {
-
-    // 出した手のところを考え中に戻す
-    displayReset();
-
-    // カードの表示のリセット
-    cardReset();
-
-    // ボタンの表示切り替え
-    navigationRestartHide();
-
-  });
 });
