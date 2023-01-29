600379fe3b94c23565139daef3a8080cac3fd435
t5a1r1ou <baw.keepon69@gmail.com>
2023-01-29 17:45:02 +0900

    もういちどボタンを削除

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,9 @@ window.addEventListener("load", () => {
   const cpuHandText = document.querySelector(".cardBox__hand--cpu");
   const cardTitle = document.querySelector(".card__title");
   const cardResult = document.querySelector(".card__result");
-  const navigationHandList = document.querySelector(".navigation__list--hands");
-  const navigationRestartList = document.querySelector(".navigation__list--restart");
   const rockButton = document.querySelector(".navigation__button--rock");
   const scissorsButton = document.querySelector(".navigation__button--scissors");
   const paperButton = document.querySelector(".navigation__button--paper");
-  const restartButton = document.querySelector(".navigation__button--restart");
 
   const displayYourHand = (param, text) => {
     yourHandImage.src = `./assets/images/${param}.png`;
@@ -23,35 +20,10 @@ window.addEventListener("load", () => {
     cpuHandText.innerHTML = text;
   };
 
-  const displayReset = () => {
-    yourHandImage.src = `./assets/images/thinking.png`;
-    yourHandText.innerHTML = "かんがえちゅう...";
-    cpuHandImage.src = `./assets/images/thinking.png`;
-    cpuHandText.innerHTML = "かんがえちゅう...";
-  };
-
   const result = () => {
     cardTitle.innerHTML = "ぽんっ！"
     cardResult.innerHTML = "あなたのかち！";
   }
-
-  const navigationHandHide = () => {
-    navigationHandList.style.display = "none";
-    navigationRestartList.style.display = "flex";
-  };
-
-  const navigationRestartHide = () => {
-    navigationHandList.style.display = "flex";
-    navigationRestartList.style.display = "none";
-  }
-
-  const cardReset = () => {
-    cardTitle.innerHTML = "じゃんけん...";
-    cardResult.innerHTML = "";
-  }
-
-  // もう一度ボタンを最初は非表示にする
-  navigationRestartList.style.display = "none";
 
   const handButtons = [rockButton, scissorsButton, paperButton];
   const handParams = ["rock", "scissors", "paper"];
@@ -73,20 +45,7 @@ window.addEventListener("load", () => {
       displayYourHand(yourHand, yourText);
       displayCpuHand(cpuHand, cpuText);
       result();
-      navigationHandHide();
     });
   }
 
-  restartButton.addEventListener("click", () => {
-
-    // 出した手のところを考え中に戻す
-    displayReset();
-
-    // カードの表示のリセット
-    cardReset();
-
-    // ボタンの表示切り替え
-    navigationRestartHide();
-
-  });
 });
