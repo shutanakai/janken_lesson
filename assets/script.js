f8e9074c5c14ae9c9998a87f994621fec4e8fa46
t5a1r1ou <baw.keepon69@gmail.com>
2023-01-29 17:58:24 +0900

    テキスト削減

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,9 @@ window.addEventListener("load", () => {
 
     // あなたの出した手
     document.querySelector(".cardBox__img--you").src = "./assets/images/rock.png";
-    document.querySelector(".cardBox__hand--you").innerHTML = "グー";
 
     // CPUの出した手
     document.querySelector(".cardBox__img--cpu").src = "./assets/images/scissors.png";
-    document.querySelector(".cardBox__hand--cpu").innerHTML = "チョキ";
 
     // じゃんけん後のカードの結果反映
     document.querySelector(".card__title").innerHTML = "ぽんっ！"
@@ -19,9 +17,7 @@ window.addEventListener("load", () => {
   // チョキを押した時
   document.querySelector(".navigation__button--scissors").addEventListener("click", () => {
     document.querySelector(".cardBox__img--you").src = "./assets/images/scissors.png";
-    document.querySelector(".cardBox__hand--you").innerHTML = "チョキ";
     document.querySelector(".cardBox__img--cpu").src = "./assets/images/paper.png";
-    document.querySelector(".cardBox__hand--cpu").innerHTML = "パー";
     document.querySelector(".card__title").innerHTML = "ぽんっ！"
     document.querySelector(".card__result").innerHTML = "あなたのかち！";
   });
@@ -29,9 +25,7 @@ window.addEventListener("load", () => {
   // パーを押した時
   document.querySelector(".navigation__button--paper").addEventListener("click", () => {
     document.querySelector(".cardBox__img--you").src = "./assets/images/paper.png";
-    document.querySelector(".cardBox__hand--you").innerHTML = "パー";
     document.querySelector(".cardBox__img--cpu").src = "./assets/images/rock.png";
-    document.querySelector(".cardBox__hand--cpu").innerHTML = "グー";
     document.querySelector(".card__title").innerHTML = "ぽんっ！"
     document.querySelector(".card__result").innerHTML = "あなたのかち！";
   });
